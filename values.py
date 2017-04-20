48a8c1dd9d0dbe04beb94ae019c7bbbf8be0cf76
Martin Breuss <martin-martin@users.noreply.github.com>
2017-04-20 19:09:30 -0400

    base code

---
+++ values.py
@@ -0,0 +1,22 @@
+# getting some values for a Triangular Irregular Network (TIN)
+import math
+
+# corner points (x,y,z)
+pt1 = (10, 20, 30)
+pt2 = (30, 20, 25)
+pt3 = (15, 30, 30)
+
+# general formula for a facet of a TIN
+#z(x,y) = b0 + b1 * x + b2 * y
+
+# factors (calculated by hand)
+b0 = -6.9
+b1 = -0.25
+b2 = 1.28
+
+# how to calculate slope and aspect of the facet
+slope = math.atan(math.sqrt(b1**2 + b2**2))
+aspect = 180 - math.atan(b2 / b1) + 90 * (b1 / abs(b1))
+
+print("slope of facet: {0}".format(slope))
+print("aspect of facet: {0}".format(aspect))
