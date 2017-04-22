999cc015622b13a9c0804e51d0d4e1f1b44dc8a2
Martin Breuss <martin-martin@users.noreply.github.com>
2017-04-21 20:01:49 -0400

    inverse calculation

--- convert_rad_deg.py
+++ convert_rad_deg.py
@@ -1,10 +1,22 @@
 import math
 
-# beautiful: https://en.wikipedia.org/wiki/User:LucasVB/Gallery#/media/File:Circle_radians.gif
 def radians_to_degrees(radians):
   """converts radians to degrees."""
   # pi radians = 180 degrees -> 1 radians = 180/pi degrees
   rads_in_degrees = ( 180 / math.pi ) * radians
   return rads_in_degrees
+  
+def degrees_to_radians(degrees):
+  """converts degrees to radians."""
+  # pi radians = 180 degrees -> 1 degree = pi/180 radians
+  degs_in_rads = ( math.pi / 180 ) * degrees
+  return degs_in_rads
+  
+#print(radians_to_degrees(math.pi))
+#print(degrees_to_radians(180))
 
-print(radians_to_degrees(math.pi))
+# And here comes the drumroll... 🥁
+# revealing the mystery:
+print(radians_to_degrees(1))
+print(degrees_to_radians(57.29578))
+# oh yes. ❤️ maths.
